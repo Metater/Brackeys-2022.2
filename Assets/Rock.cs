2d77d384ca851576e554d1fe3f6a634814c491e2
Connor <61102529+Metater@users.noreply.github.com>
2022-08-21 22:17:08 -0400

    Rock stuff

--- Assets/Rock.cs
+++ Assets/Rock.cs
@@ -8,16 +8,32 @@ public class Rock : MonoBehaviour
     [SerializeField] private float rockSpeed;
     [SerializeField] private float rockRadius;
     [SerializeField] private float rockSmoothSpeed;
-
+    [Space]
     [SerializeField] private GameObject rockOrbitDotPrefab;
     [SerializeField] private int rockOrbitDotsCount;
+    [Space]
+    [SerializeField] private bool pointAwayFromPlayer;
+    [SerializeField] private bool hasRandomTumbleSpeed;
+    [SerializeField] private Vector2 tubleSpeedRange;
 
     private Vector3 rockVelocity;
+    private float tumbleSpeed;
     private List<GameObject> orbitDots;
+
+    private float radians = 0f;
 
     private void Awake()
     {
         orbitDots = new();
+
+        if (hasRandomTumbleSpeed)
+        {
+            tumbleSpeed = Random.Range(tubleSpeedRange.x, tubleSpeedRange.y);
+        }
+        else
+        {
+            tumbleSpeed = tubleSpeedRange.x;
+        }
     }
 
     private void Start()
@@ -27,10 +43,24 @@ public class Rock : MonoBehaviour
 
     private void Update()
     {
-        // Make rock speed units/sec, not arc/sec
-        float rockTime = Time.time * rockSpeed;
-        Vector3 rockyPos = player.position + new Vector3(Mathf.Cos(rockTime), Mathf.Sin(rockTime)) * rockRadius;
+        if (pointAwayFromPlayer)
+        {
+            var angle = AngleBetweenTwoPoints(transform.position, player.position);
+            transform.localEulerAngles = new Vector3(0f, 0f, angle - 90f);
+        }
+        else
+        {
+            transform.localEulerAngles = new Vector3(0f, 0f, transform.localEulerAngles.z + Time.deltaTime * tumbleSpeed);
+        }
+
+        radians += (Time.deltaTime * rockSpeed) / rockRadius;
+        Vector3 rockyPos = player.position + new Vector3(Mathf.Cos(radians), Mathf.Sin(radians)) * rockRadius;
         transform.position = Vector3.SmoothDamp(transform.position, rockyPos, ref rockVelocity, rockSmoothSpeed);
+    }
+
+    private float AngleBetweenTwoPoints(Vector3 a, Vector3 b)
+    {
+        return Mathf.Atan2(a.y - b.y, a.x - b.x) * Mathf.Rad2Deg;
     }
 
     public void RefreshOrbitDots()
