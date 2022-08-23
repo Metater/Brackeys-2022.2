239c028b1106eb77611ead09e4c269fac421b3bf
DankKid <jmanholt929@gmail.com>
2022-08-22 21:21:39 -0400

    Volume Control & Spider

--- Assets/TitleManager.cs
+++ Assets/TitleManager.cs
@@ -2,15 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+
+
 public class TitleManager : MonoBehaviour
 {
     [SerializeField] Animator anim;
-
+    [SerializeField] Slider slider;
+    [SerializeField] AudioMixer audioMixer;
     public void PlayGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
-
 
     bool optionsIsOpened = false;
 
@@ -31,10 +35,39 @@ public class TitleManager : MonoBehaviour
         anim.SetTrigger("CloseOP");
     }
 
-
     public void QuitGame()
     {
         Application.Quit();
         Debug.Log("Quit");
     }
+
+    private void Start()
+    {
+        SetVolumeOnStart();
+    }
+    private void SetVolumeOnStart()
+    {
+        float volume = 0;
+        if (PlayerPrefs.HasKey("volume"))
+            volume = PlayerPrefs.GetFloat("volume");
+        slider.value = volume;
+        SetVolume(volume);
+    }
+
+    public void SetVolume(float volume)
+    {
+        PlayerPrefs.SetFloat("volume", volume);
+        PlayerPrefs.Save();
+
+        audioMixer.SetFloat("Volume", volume);
+
+        if (volume == -20)
+        {
+            audioMixer.SetFloat("Volume", -80);
+        }
+    }
+
+
+
+
 }
