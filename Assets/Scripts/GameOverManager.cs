7ae9c44c630bbfe40ba51a7c84d86e818a72f6c6
DankKid <jmanholt929@gmail.com>
2022-08-28 02:08:45 -0400

    Final Hopefully for real

--- Assets/Scripts/GameOverManager.cs
+++ Assets/Scripts/GameOverManager.cs
@@ -8,10 +8,15 @@ public class GameOverManager : MonoBehaviour
     [SerializeField] TMP_Text bestWave, currentWave;
     [SerializeField] GameObject gameOverScreen;
 
+    [SerializeField] WaveManager waveManager;
     public void GameOver()
     {
+        if(waveManager.wave >= PlayerPrefs.GetInt("BestWave"))
+        {
+            PlayerPrefs.SetInt("BestWave", waveManager.wave);
+        }
         bestWave.text = "Wave: " + PlayerPrefs.GetInt("BestWave");
-        currentWave.text = "Wave: " + "";
+        currentWave.text = "Wave: " + waveManager.wave;
         gameOverScreen.SetActive(true);
     }
 
